47817b585d811b4ca761ef1847155996683d4544
zcroft27 <croft.z@northeastern.edu>
2023-12-26 18:14:15 -0500

    log data function and git ignore file

--- HomeworkTimer.py
+++ HomeworkTimer.py
@@ -1,9 +1,20 @@
+timerRunning = false
+
 def main():
     #TODO
     return 1
 
+def log_data(duration, description, date):
+    my_data = open("my_data.txt", "a")
+
+    my_data.write(f"Date: {date}, Duration: {duration}, Description: {description}")
+
+    my_data.close()
+
+def update_timer():
+    return 1
 
 
-
+        
 
 
