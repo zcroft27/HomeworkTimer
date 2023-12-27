68874f5d8dcfa047e35c226b03f702efe5f18b98
zcroft27 <croft.z@northeastern.edu>
2023-12-27 15:22:13 -0500

    adds durations to totalDuration variable, added hotkey to print totalDuration

--- HomeworkTimer.py
+++ HomeworkTimer.py
@@ -3,16 +3,12 @@ import time
 from datetime import timedelta
 import keyboard
 import sys
+from win10toast import ToastNotifier
 
 timerRunning = False
 start = None
 end = None
-
-
-def main():
-    #TODO
-    return 1
-
+totalDuration = timedelta()
 
 def log_data(duration, description, date):
     my_data = open("my_data.txt", "a")
@@ -22,26 +18,23 @@ def log_data(duration, description, date):
     my_data.close()
 
 def update_timer():
-    global timerRunning, end, start
+    global timerRunning, end, start, totalDuration
     if timerRunning:
         end = datetime.now()
         # Format the datetime object as a string with both date and time
         formatted_datetime = start.strftime("%Y-%m-%d %H:%M")
         log_data((end-start), "2test", formatted_datetime)
+        totalDuration += (end-start)
     else:
         start = datetime.now()
     timerRunning = not timerRunning
   
-# get current time
-    # while loop until hotkey is pressed again
-    # get end end time 
-    # find difference in times
-    # prompt user to write description of session
-    # write to file
-
+def printTotalDuration():
+    print(totalDuration)
 
 keyboard.add_hotkey('ctrl+alt+a', update_timer)
 keyboard.add_hotkey('ctrl+alt+q', sys.exit)
+keyboard.add_hotkey('ctrl+alt+y', printTotalDuration)
 keyboard.wait()
 
 
