e342bfb2ea5e8e81901d756a87f48e5d2786b1da
zcroft27 <croft.z@northeastern.edu>
2023-12-27 16:36:07 -0500

    parseJsonDuration, updateJson, ToastTotalDuration, data.json

--- HomeworkTimer.py
+++ HomeworkTimer.py
@@ -4,11 +4,17 @@ from datetime import timedelta
 import keyboard
 import sys
 from win10toast import ToastNotifier
+import json
 
 timerRunning = False
 start = None
 end = None
-totalDuration = timedelta()
+currentDuration = timedelta()
+
+
+jsonFilePath = "data.json"
+
+
 
 def log_data(duration, description, date):
     my_data = open("my_data.txt", "a")
@@ -18,24 +24,53 @@ def log_data(duration, description, date):
     my_data.close()
 
 def update_timer():
-    global timerRunning, end, start, totalDuration
+    global timerRunning, end, start, currentDuration
     if timerRunning:
         end = datetime.now()
         # Format the datetime object as a string with both date and time
         formatted_datetime = start.strftime("%Y-%m-%d %H:%M")
         log_data((end-start), "2test", formatted_datetime)
-        totalDuration += (end-start)
+        currentDuration = (end-start)
     else:
         start = datetime.now()
     timerRunning = not timerRunning
   
-def printTotalDuration():
+def ToastTotalDuration(dur):
     #prints total duration
-    print(totalDuration)
+    toaster = ToastNotifier()
+    toaster.show_toast("Total Duration: ", str(dur))
+
+def parseJsonDuration(dur):
+    hours, minutes, seconds = map(float, dur.split(":"))
+    return timedelta(hours=hours, minutes=minutes, seconds=seconds)
+
+def updateJson():
+    global currentDuration
+    with open(jsonFilePath, 'r') as jsonFile:
+        jsonData = json.load(jsonFile)
+
+    sumDuration = 0
+
+    for key in jsonData:
+        if key == "duration":
+            oldTotalDuration = parseJsonDuration(jsonData["duration"])
+            sumDuration = currentDuration + oldTotalDuration
+            jsonData["duration"] = str(sumDuration)
+            ToastTotalDuration(sumDuration)
+
+    with open(jsonFilePath, 'w') as jsonFile:
+        json.dump(jsonData, jsonFile, indent=2)
+
+    
+
+
+#toast notification indicating timer has started
+    #toast notification indicating timer has stopped
+        # in notification, displays "press 1, 2, or 3 for homework, research, lecture" and wait for user input to close notification   
 
 keyboard.add_hotkey('ctrl+alt+a', update_timer)
 keyboard.add_hotkey('ctrl+alt+q', sys.exit)
-keyboard.add_hotkey('ctrl+alt+y', printTotalDuration)
+keyboard.add_hotkey('ctrl+alt+i', updateJson)
 keyboard.wait()
 
 
